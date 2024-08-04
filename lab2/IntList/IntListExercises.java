2b7cb1632014bb69e4e3f7d3091020cff8afe882
xuan-lgbq <bxl307@student.bham.ak.uk>
2024-08-04 17:55:36 +0800

    lab2

---
+++ lab2/IntList/IntListExercises.java
@@ -0,0 +1,94 @@
+package IntList;
+
+public class IntListExercises {
+
+    /**
+     * Part A: (Buggy) mutative method that adds a constant C to each
+     * element of an IntList
+     *
+     * @param lst IntList from Lecture
+     */
+    public static void addConstant(IntList lst, int c) {
+        IntList head = lst;
+        while (head.rest != null) {
+            head.first += c;
+            head = head.rest;
+        }
+        head.first += c;
+    }
+
+    /**
+     * Part B: Buggy method that sets node.first to zero if
+     * the max value in the list starting at node has the same
+     * first and last digit, for every node in L
+     *
+     * @param L IntList from Lecture
+     */
+    public static void setToZeroIfMaxFEL(IntList L) {
+        IntList p = L;
+        while (p != null) {
+            /** int currentMax=max(p);
+             *boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+             * if( firstEqualsLast){
+             * p.first=0;
+             }*/
+            //where we can use this code to check our function respectively to make sure they are valid.
+            //Moreover, it is more convenient for us to debug them.
+            if (firstDigitEqualsLastDigit(max(p))) {
+                p.first = 0;
+            }
+            p = p.rest;
+        }
+    }
+
+    /**
+     * Returns the max value in the IntList starting at L.
+     */
+    public static int max(IntList L) {
+        int max = L.first;
+        IntList p = L.rest;
+        while (p != null) {
+            if (p.first > max) {
+                max = p.first;
+            }
+            p = p.rest;
+        }
+        return max;
+    }
+
+    /**
+     * Returns true if the last digit of x is equal to
+     * the first digit of x.
+     */
+    public static boolean firstDigitEqualsLastDigit(int x) {
+        int lastDigit = x % 10;
+        while (x >= 10) {
+            x = x / 10;
+        }
+        int firstDigit = x % 10;
+        return firstDigit == lastDigit;
+    }
+
+    /**
+     * Part C: (Buggy) mutative method that squares each prime
+     * element of the IntList.
+     *
+     * @param lst IntList from Lecture
+     * @return True if there was an update to the list
+     */
+    public static boolean squarePrimes(IntList lst) {
+        // Base Case: we have reached the end of the list
+        if (lst == null) {
+            return false;
+        }
+        boolean hasPrime = false;
+        boolean currElemIsPrime = Primes.isPrime(lst.first);
+        if (currElemIsPrime) {
+            lst.first *= lst.first;
+            hasPrime = true;
+        }
+    return squarePrimes(lst.rest) || hasPrime;
+        //The order matters, if we put squarePrimes first then the code will check the squarePrimes first and then check the hasPrime.
+        //which means until we recurse the whole list then we use the order of "return".
+    }
+}
